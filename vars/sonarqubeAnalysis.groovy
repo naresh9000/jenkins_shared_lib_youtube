c1a2077284f007b5d53423f5b8ac66a570c8a76b
naresh9000 <naresh.naresh.k7@gmail.com>
2024-03-30 15:29:10 +0530

    addding functions

--- vars/sonarqubeAnalysis.groovy
+++ vars/sonarqubeAnalysis.groovy
@@ -1,9 +1,10 @@
 def call() {
     withSonarQubeEnv('sonar') {
-    sh ''' $SCANNER_HOME \
+    sh  ''' $SCANNER_HOME \
         -Dsonar.projectKey=youtube \
         -Dsonar.sources=. \
         -Dsonar.host.url=http://ec2-3-234-177-208.compute-1.amazonaws.com:9000 \
-        -Dsonar.login=squ_6c43bf967ebe25a07a8ac73b8e3729bfa5356941 '''   
+        -Dsonar.login=squ_6c43bf967ebe25a07a8ac73b8e3729bfa5356941 
+        '''   
     }
 }
